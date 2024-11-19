665e38a3ffbae002badee5682f69fddcb972dbc8
Taranya Elangovan <taranya@hotmail.co.uk>
2024-11-19 00:38:34 +0000

    modifying filename-based classification to be based on multiple variations and use cleaned filename

--- src/classifier.py
+++ src/classifier.py
@@ -1,4 +1,22 @@
 from werkzeug.datastructures import FileStorage
+from src.constants import FILE_TYPE_VARIATIONS
+
+
+def check_substring(search_string, search_list):
+    """
+    checks if any of the possible string variations for in search_list are present in the search_string
+    """
+    return any(substring in search_string for substring in search_list)
+
+
+def clean_filename(filename: str = ""):
+    """
+    cleans filename to remove any spaces/underscores/hyphens, and converts to lowercase
+    """
+    filename_cleaned = (
+        filename.replace(" ", "").replace("_", "").replace("-", "").lower()
+    )
+    return filename_cleaned
 
 
 def classify_file(file: FileStorage):
@@ -7,16 +25,14 @@ def classify_file(file: FileStorage):
     # * Initially simple rule-based
     # ! Make more robust - add in more rules and classification based on file content
     """
-    filename = file.filename.lower()
+
+    filename_cleaned = clean_filename(file.filename)
+
+    for key in FILE_TYPE_VARIATIONS:
+        if check_substring(filename_cleaned, FILE_TYPE_VARIATIONS[key]):
+            return key
+
+    # ! Add in handling for files that have not already been classified - check here for file contents if not already categorised based on name
     # file_bytes = file.read()
-
-    if "drivers_license" in filename:
-        return "drivers_licence"
-
-    if "bank_statement" in filename:
-        return "bank_statement"
-
-    if "invoice" in filename:
-        return "invoice"
 
     return "unknown file"
