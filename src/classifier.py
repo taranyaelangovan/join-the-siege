5d17dfa340af3f9e616f7a625d573bcd1514ef3f
Taranya Elangovan <taranya@hotmail.co.uk>
2024-11-20 00:06:28 +0000

    considering similar cleaned substring matches for classification if there are no exact substring matches

--- src/classifier.py
+++ src/classifier.py
@@ -1,5 +1,6 @@
 from werkzeug.datastructures import FileStorage
 from src.constants import FILE_TYPE_VARIATIONS
+from jellyfish import jaro_winkler_similarity
 
 
 def check_substring(search_string, search_list):
@@ -9,13 +10,30 @@ def check_substring(search_string, search_list):
     return any(substring in search_string for substring in search_list)
 
 
+def check_similar_substring(search_string, search_list):
+    """
+    checks if any of the possible string variations for in search_list are present in the search_string
+    """
+
+    for substring in search_list:
+        print(
+            f"Similarity between {search_string} and *{substring}*: {jaro_winkler_similarity(substring,search_string)}"
+        )
+
+    return any(
+        jaro_winkler_similarity(substring, search_string) > 0.85
+        for substring in search_list
+    )
+
+
 def clean_filename(filename: str = ""):
     """
     cleans filename to remove any spaces/underscores/hyphens, and converts to lowercase
     """
-    filename_cleaned = (
-        filename.replace(" ", "").replace("_", "").replace("-", "").lower()
-    )
+    # filename_cleaned = (
+    #     filename.replace(" ", "").replace("_", "").replace("-", "").lower()
+    # )
+    filename_cleaned = "".join(x for x in filename if x.isalpha())
     return filename_cleaned
 
 
@@ -30,6 +48,10 @@ def classify_file(file: FileStorage):
 
     for key in FILE_TYPE_VARIATIONS:
         if check_substring(filename_cleaned, FILE_TYPE_VARIATIONS[key]):
+            return key
+
+    for key in FILE_TYPE_VARIATIONS:
+        if check_similar_substring(filename_cleaned, FILE_TYPE_VARIATIONS[key]):
             return key
 
     # ! Add in handling for files that have not already been classified - check here for file contents if not already categorised based on name
